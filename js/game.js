e0c1aba3cb4d048edb9be6430638996e00f9baed
Mai <mxngyn@gmail.com>
2015-03-27 16:18:29 -0400

    Add high score in localStorage

--- js/game.js
+++ js/game.js
@@ -11,6 +11,9 @@ var Game = (function() {
   function Game() {
     this.tiles = [];
     this.board = this.seed(("0000000000000000").split("")).map(function(currentValue){ return parseInt(currentValue) });
+    this.score = 0;
+    this.won = false;
+    this.lose = false;
   }
 
   Game.prototype.seed = function(array) {
@@ -82,6 +85,11 @@ var Game = (function() {
         boardHtml += "</div>";
       };
     }
+    if(this.won === true) {
+      boardHtml += "<div id='winner'> Yay! You won! </div>"
+    }
+    boardHtml += "<div id='score'> Score: " + this.score + "</div>"
+    boardHtml += "<div id='score'> Highest Score: " + window.localStorage.score + "</div>"
     document.getElementById("game-container").innerHTML = boardHtml;
   }
 
@@ -108,13 +116,33 @@ var Game = (function() {
     }
   }
 
+  Game.prototype.startNewGame = function() {
+    this.tiles = [];
+    this.board = this.seed(("0000000000000000").split("")).map(function(currentValue){ return parseInt(currentValue) });
+
+  }
+
   Game.prototype.checkWin = function() {
     for (i = 0; i < this.board.length; i++) {
       if (this.board[i] == 256 ){
-        alert("YOU HAVE WON THIS ITERATION OF 256...");
+        this.won = true;
+        this.toHtml();
       };
     }
   }
+
+  // Game.prototype.gameOver = function() {
+  //   if(this.chaChaSlide(37).join() === this.chaChaSlide(39).join() && this.chaChaSlide(37).join() === this.chaChaSlide(38).join() && this.chaChaSlide(37).join() === this.chaChaSlide(40).join()) {
+  //     console.log("Game over");
+  //     return this.lose = true;
+  //   }
+  // }
+
+  // Game.prototype.checkLose = function() {
+  //   if(this.gameOver()) {
+  //     alert("Sorry! There are no more available moves left.");
+  //   }
+  // }
 
   Game.prototype.chaChaSlide = function(direction) {
     var self = this;
@@ -131,6 +159,7 @@ var Game = (function() {
           if(filteredRow.length > 1) {
             for(i = 0; i < filteredRow.length-1; i++){
               if(filteredRow[i] === filteredRow[i+1]){
+                self.score += filteredRow[i] * 2;
                 filteredRow.splice(i, 2, (filteredRow[i] * 2));
                 filteredRow.push(0);
               }
@@ -141,18 +170,20 @@ var Game = (function() {
           }
           return filteredRow;
         });
-      console.log(oldBoard);
       self.setRows(mappedRows);
-      console.log(self.board);
       if(oldBoard.join() === self.board.join()) {
         console.log("same");
       }
       else {
         self.randomSpawn();
       }
-      self.toHtml();
       self.checkWin();
-      console.log("*******");
+      // self.checkLose();
+      if(parseInt(window.localStorage.score) < self.score) {
+        window.localStorage.score = self.score;
+      }
+      self.toHtml();
+      return self.board;
       break;
       case 39:
       var oldBoard = self.board;
@@ -161,6 +192,7 @@ var Game = (function() {
         if(filteredRow.length > 1) {
           for(i = 0; i < filteredRow.length-1; i++){
             if(filteredRow[i] === filteredRow[i+1]){
+              self.score += filteredRow[i] * 2;
               filteredRow.splice(i, 2, (filteredRow[i] * 2));
               filteredRow.push(0);
             }
@@ -178,8 +210,13 @@ var Game = (function() {
       else {
         self.randomSpawn();
       }
-      self.toHtml();
       self.checkWin();
+      // self.checkLose();
+      if(parseInt(window.localStorage.score) < self.score) {
+        window.localStorage.score = self.score;
+      }
+      self.toHtml();
+      return self.board;
       break;
       case 38:
       var oldBoard = self.board;
@@ -188,6 +225,7 @@ var Game = (function() {
         if(filteredCol.length > 1) {
           for(i = 0; i < filteredCol.length-1; i++){
             if(filteredCol[i] === filteredCol[i+1]){
+              self.score += filteredCol[i] * 2;
               filteredCol.splice(i, 2, (filteredCol[i] * 2));
               filteredCol.push(0);
             }
@@ -205,8 +243,13 @@ var Game = (function() {
       else {
         self.randomSpawn();
       }
-      self.toHtml();
       self.checkWin();
+      // self.checkLose();
+      if(parseInt(window.localStorage.score) < self.score) {
+        window.localStorage.score = self.score;
+      }
+      self.toHtml();
+      return self.board;
       break;
       case 40:
       var oldBoard = self.board;
@@ -215,6 +258,7 @@ var Game = (function() {
         if(filteredCol.length > 1) {
           for(i = 0; i < filteredCol.length-1; i++){
             if(filteredCol[i] === filteredCol[i+1]){
+              self.score += filteredCol[i] * 2;
               filteredCol.splice(i, 2, (filteredCol[i] * 2));
               filteredCol.push(0);
             }
@@ -232,8 +276,13 @@ var Game = (function() {
       else {
         self.randomSpawn();
       }
-      self.toHtml();
       self.checkWin();
+      if(parseInt(window.localStorage.score) < self.score) {
+        window.localStorage.score = self.score;
+      }
+      self.toHtml();
+      // self.checkLose();
+      return self.board;
       break;
     }
   }
